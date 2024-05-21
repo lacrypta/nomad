70907d1526695e09ddf5eb7a0d6a8004be558d02
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2024-05-21 12:01:07 -0300

    Clean up TODOs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 'use strict';
 
-// TODO: ADD MISSING SHIMS
 // TODO: ANALYZE Iterator AND OTHER HIDDEN OBJECTS
 
-// TODO: change "namespace" to "enclosure" or something along those lines
+// TODO: Change "namespace" to "enclosure" or something along those lines
 
-// TODO: for worker.js, only use eslint JS rules, avoid eslint ts rules (otoh: pay attention to them as well, no matter how whiny they are)
+// TODO: For worker.js, only use eslint JS rules, avoid eslint ts rules (otoh: pay attention to them as well, no matter how whiny they are)
 
 import { Dependency } from './dependency';
 import { NomadVM, NomadVMNamespace } from './nomadvm';
