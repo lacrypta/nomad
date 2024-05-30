307b07a3146da23ef71d32270bb73b780c348711
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2024-05-30 00:49:48 -0300

    Add test helpers

---
+++ test/helpers.ts
@@ -0,0 +1,71 @@
+// nomadvm: The Nomad Virtual Machine reference implementation
+//
+// MIT License
+//
+// Copyright (c) 2024 La Crypta
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+export type TestCaseInput<T extends (...args: any) => any> =
+  | {
+      input: Parameters<T>;
+      expected: ReturnType<T>;
+      error: null;
+    }
+  | {
+      input: Parameters<T>;
+      expected: null;
+      error: Error;
+    };
+
+export type TestCases<T extends (...args: any) => any> = {
+  [name: string]: TestCaseInput<T>;
+};
+
+export const testAll: <T extends (...args: any) => any>(
+  it: jest.It,
+  func: T,
+  cases: Record<string, any>,
+) => void = <T extends (...args: any) => any>(
+  it: jest.It,
+  func: T,
+  cases: Record<string, any>,
+): void => {
+  describe(`${func.name}()`, (): void => {
+    it.each(
+      Object.entries(cases as TestCases<T>).map(
+        ([name, testCase]: [string, TestCaseInput<T>]): TestCaseInput<T> & {
+          name: string;
+        } => {
+          return { name, ...testCase };
+        },
+      ),
+    )("$name", ({ input, expected, error }: TestCaseInput<T>): void => {
+      if (error instanceof Error) {
+        expect((): void => {
+          func(...input);
+        }).toThrow(error);
+      } else {
+        expect(JSON.stringify(func(...input))).toStrictEqual(
+          JSON.stringify(expected),
+        );
+      }
+    });
+  });
+};
